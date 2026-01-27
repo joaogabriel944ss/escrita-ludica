94ffcb1bd57e83a0dba9d09d0bd7ca2cf3314155
Joao Gabriel (51LV4) <joaogabriel944ss@gmail.com>
2026-01-27 16:05:54 -0300

    Version complete - 27/01/2026 at 15:40 action: [ Versão Completa e Revisada, testada nas rotas de Login Cadastro e Livros ] done by: [João Gabriel]

--- FRONTEND/view/react/cypresstest/cypress/e2e/ludica.cy.js
+++ FRONTEND/view/react/cypresstest/cypress/e2e/ludica.cy.js
@@ -1,21 +1,37 @@
-describe('template spec', () => {
-  it('passes', () => {
+describe('Fluxo Completo: Cadastro, Login e Criação de Livro', () => {
+  it('Deve preencher o título e a sinopse corretamente', () => {
+   
     cy.visit('https://escrita-ludica-lfix.onrender.com')
 
-    cy.get('[placeholder="Nome"]').type('cypressname')
-
-cy.get('[placeholder="Email"]').type('cypressemail@example.com')
-
-cy.get('[placeholder="Senha"]').type('cypresspassword')
-
-cy.get('.bg-blue-600').click()
-
-cy.get('.cursor-poiter').click()
-
-cy.get('[placeholder="Email"]').type('cypressemail@example.com')
-
-cy.get('[placeholder="Senha"]').type('cypresspassword')
-  })
-
   
+    cy.get('[placeholder="Nome"]').type('cypressname')
+    cy.get('[placeholder="Email"]').type('cypressemail@example.com')
+    cy.get('[placeholder="Senha"]').type('cypresspassword')
+    cy.get('.bg-blue-600').click()
+
+   
+    cy.get('.cursor-poiter').click()
+
+    // 4. Login
+    cy.get('[placeholder="Email"]').type('cypressemail@example.com')
+    cy.get('[placeholder="Senha"]').type('cypresspassword')
+    cy.get('.bg-blue-700').click()
+
+   
+    cy.url().should('include', '/livros')
+    
+   
+    cy.get('input[placeholder="Título"]')
+      .should('be.visible')
+      .type('Meu Novo Livro Automático', { delay: 100 })
+
+    
+    cy.get('textarea[placeholder="Sinopse"]')
+      .should('be.visible')
+      .type('Esta sinopse foi escrita pelo Cypress para validar o formulário.', { delay: 50 })
+
+    
+    cy.get('select').select(1) 
+    cy.get('button').contains('Salvar').click()
+  })
 })
